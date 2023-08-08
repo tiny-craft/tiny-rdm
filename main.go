6937939916bc0192e059aedbd03de0ba4c7acd70
tiny-craft <137850705+tiny-craft@users.noreply.github.com>
2023-08-08 08:45:26 +0800

    feat: add github actions for release macOS app

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"github.com/wailsapp/wails/v2/pkg/menu"
 	"github.com/wailsapp/wails/v2/pkg/options"
 	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
+	"github.com/wailsapp/wails/v2/pkg/options/linux"
 	"github.com/wailsapp/wails/v2/pkg/options/mac"
 	"runtime"
 	"tinyrdm/backend/services"
@@ -71,6 +72,10 @@ func main() {
 			},
 			//WebviewIsTransparent: true,
 			//WindowIsTranslucent:  true,
+		},
+		Linux: &linux.Options{
+			Icon:             icon,
+			WebviewGpuPolicy: linux.WebviewGpuPolicyOnDemand,
 		},
 	})
 
