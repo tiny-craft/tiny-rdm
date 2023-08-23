0b8ee4bd91dff10ab9e546c50a5f705d9acc60f6
tiny-craft <137850705+tiny-craft@users.noreply.github.com>
2023-08-23 16:38:56 +0800

    perf: update key and key layer icon

--- frontend/src/components/icons/Layer.vue
+++ frontend/src/components/icons/Layer.vue
@@ -12,35 +12,21 @@ const props = defineProps({
 </script>
 
 <template>
-    <svg fill="none" viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg">
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+    >
         <path
+            d="M10 20H4a2 2 0 0 1-2-2V5c0-1.1.9-2 2-2h3.93a2 2 0 0 1 1.66.9l.82 1.2a2 2 0 0 0 1.66.9H20a2 2 0 0 1 2 2v2"
             :fill="props.fillColor"
-            d="M4 11.9143L24 19L44 11.9143L24 5L4 11.9143Z"
-            stroke="currentColor"
-            stroke-linejoin="round"
-            stroke-width="3"
         />
-        <path
-            :stroke-width="props.strokeWidth"
-            d="M4 20L24 27L44 20"
-            stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-        />
-        <path
-            :stroke-width="props.strokeWidth"
-            d="M4 28L24 35L44 28"
-            stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-        />
-        <path
-            :stroke-width="props.strokeWidth"
-            d="M4 36L24 43L44 36"
-            stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-        />
+        <circle cx="16" cy="20" r="2" :fill="props.fillColor" />
+        <path d="m22 14-4.5 4.5" />
+        <path d="m21 15 1 1" />
     </svg>
 </template>
 
