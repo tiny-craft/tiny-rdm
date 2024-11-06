b601ba255ba990584bef7e06a3270b4c119b84ea
Lykin <137850705+tiny-craft@users.noreply.github.com>
2024-11-06 16:17:29 +0800

    pref: place the tooltip of switch bottom place at the bottom by default

--- frontend/src/components/common/SwitchButton.vue
+++ frontend/src/components/common/SwitchButton.vue
@@ -12,6 +12,10 @@ const props = defineProps({
     },
     icons: Array,
     tTooltips: Array,
+    tTooltipPlacement: {
+        type: String,
+        default: 'bottom',
+    },
     iconSize: {
         type: [Number, String],
         default: 20,
@@ -45,6 +49,7 @@ const handleSwitch = (idx) => {
             v-for="(icon, i) in props.icons"
             :key="i"
             :disabled="!(props.tTooltips && props.tTooltips[i])"
+            :placement="props.tTooltipPlacement"
             :show-arrow="false">
             <template #trigger>
                 <n-button :focusable="false" :size="props.size" :tertiary="i !== props.value" @click="handleSwitch(i)">
