2aa302c65260c3722fb978cc32c51a3f414320ae
Lykin <137850705+tiny-craft@users.noreply.github.com>
2025-05-08 18:10:48 +0800

    chore: swap export and import icon

--- frontend/src/components/icons/Export.vue
+++ frontend/src/components/icons/Export.vue
@@ -11,19 +11,19 @@ const props = defineProps({
     <svg fill="none" viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg">
         <path
             :stroke-width="props.strokeWidth"
-            d="M6 24V42H42V24"
+            d="M6 24.0083V42H42V24"
             stroke="currentColor"
             stroke-linecap="round"
             stroke-linejoin="round" />
         <path
             :stroke-width="props.strokeWidth"
-            d="M33 15L24 6L15 15"
+            d="M33 23L24 32L15 23"
             stroke="currentColor"
             stroke-linecap="round"
             stroke-linejoin="round" />
         <path
             :stroke-width="props.strokeWidth"
-            d="M24 6V32"
+            d="M23.9917 6V32"
             stroke="currentColor"
             stroke-linecap="round"
             stroke-linejoin="round" />
