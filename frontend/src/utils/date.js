7e79355be0ef7361ded98c7eed32cc8b995fb902
Lykin <137850705+tiny-craft@users.noreply.github.com>
2025-05-28 16:25:36 +0800

    fix: ttl format display incorrect when days greater then 30 (#448)

--- frontend/src/utils/date.js
+++ frontend/src/utils/date.js
@@ -1,5 +1,5 @@
-import dayjs from 'dayjs'
 import { i18nGlobal } from '@/utils/i18n.js'
+import { padStart } from 'lodash'
 
 /**
  * convert seconds number to human-readable string
@@ -7,11 +7,14 @@ import { i18nGlobal } from '@/utils/i18n.js'
  * @return {string}
  */
 export const toHumanReadable = (duration) => {
-    const dur = dayjs.duration(duration, 'seconds')
-    const days = Math.floor(dur.asDays())
+    const days = Math.floor(duration / 86400)
+    const hours = Math.floor((duration % 86400) / 3600)
+    const minutes = Math.floor((duration % 3600) / 60)
+    const seconds = duration % 60
+    const time = `${padStart(hours, 2, '0')}:${padStart(minutes, 2, '0')}:${padStart(seconds, 2, '0')}`
     if (days > 0) {
-        return days + i18nGlobal.t('common.unit_day') + ' ' + dur.format('HH:mm:ss')
+        return days + i18nGlobal.t('common.unit_day') + ' ' + time
     } else {
-        return dur.format('HH:mm:ss')
+        return time
     }
 }
