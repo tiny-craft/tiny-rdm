0fdb26fb08f740f6efe2a12e46602c6831619015
Lykin <137850705+tiny-craft@users.noreply.github.com>
2025-11-27 16:00:45 +0800

    pref: remove last empty line in bit set view (#503)

--- backend/utils/convert/bitset_convert.go
+++ backend/utils/convert/bitset_convert.go
@@ -20,16 +20,32 @@ func (BitSetConvert) Encode(str string) (string, bool) {
 	str = strings.ReplaceAll(str, "\r", "\n")   // CR → LF
 
 	lines := strings.Split(str, "\n")
-	bytes := EncodeToRedisBitset(lines)
+	bytes := encodeToRedisBitset(lines)
 	result.Write(bytes)
 
 	return result.String(), true
 }
 
-// EncodeToRedisBitset encodes a list of strings with integers (positions) into a Redis bitset byte array.
+func (BitSetConvert) Decode(str string) (string, bool) {
+	bitset := getBitSet([]byte(str))
+
+	var binBuilder strings.Builder
+	for pos, value := range bitset {
+		if value {
+			if binBuilder.Len() > 0 {
+				binBuilder.WriteByte('\n')
+			}
+			binBuilder.WriteString(fmt.Sprintf("%d", pos))
+		}
+	}
+
+	return binBuilder.String(), true
+}
+
+// encodeToRedisBitset encodes a list of strings with integers (positions) into a Redis bitset byte array.
 // The bit at position 'n' will be set to 1 if n is in the input array.
 // The resulting byte slice can be stored in Redis using SET command.
-func EncodeToRedisBitset(numbers []string) []byte {
+func encodeToRedisBitset(numbers []string) []byte {
 	if len(numbers) == 0 {
 		return []byte{}
 	}
@@ -75,26 +91,12 @@ func EncodeToRedisBitset(numbers []string) []byte {
 	return bitset
 }
 
-func (BitSetConvert) Decode(str string) (string, bool) {
-	bitset := getBitSet([]byte(str))
-
-	var binBuilder strings.Builder
-	for key, value := range bitset {
-		if value {
-			binBuilder.WriteString(fmt.Sprintf("%v\n", key))
-			//binBuilder.WriteString(fmt.Sprintf("Bit %v = %v \n", key, value))
-		}
-	}
-
-	return binBuilder.String(), true
-}
-
 func getBitSet(redisResponse []byte) []bool {
 	bitset := make([]bool, len(redisResponse)*8)
 	for i := range redisResponse {
 		for j := 7; j >= 0; j-- {
-			bit_n := uint(i*8 + (7 - j))
-			bitset[bit_n] = (redisResponse[i] & (1 << uint(j))) > 0
+			bitPos := uint(i*8 + (7 - j))
+			bitset[bitPos] = (redisResponse[i] & (1 << uint(j))) > 0
 		}
 	}
 	return bitset
